e07467baf94e00494f6d305e6df628a0a28658af
christopher-lamkin <cjlamkin64@gmail.com>
2017-05-30 10:31:32 -0700

    add routers to index file

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:123@ds023570.mlab.com:23570/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
